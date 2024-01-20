e3d48cbd323bb3d3be5db2ac710f265a232eecec
kanhui <hui.kan@maixunbytes.com>
2024-01-20 13:57:21 +0800

    fix

--- githubhosts/main.py
+++ githubhosts/main.py
@@ -1,3 +1,4 @@
+import os
 from loguru import logger
 from githubhosts.ali import AliPing
 from githubhosts.utils import ip_ping, write_hosts
@@ -37,8 +38,7 @@ HOSTS = [
     "raw.github.com",
     "copilot-proxy.githubusercontent.com",
 ]
-
-if __name__ == "__main__":
+def get_all_host_ip():
     host_ips = []
     for host in HOSTS:
         ips = list(set(AliPing().get_all_ip(host)))
@@ -55,4 +55,35 @@ if __name__ == "__main__":
             sorted_ips = sorted(valid_ips, key=lambda x: x[1])
             ip = sorted_ips[0][0]
             host_ips.append((ip, host))
+    return host_ips
+
+def update_smartdns(host_ips):
+    
+    # 重写配置文件
+    smarddns_conf_path = "/etc/smartdns/address.conf"
+    config_start_flag = "# Github hosts start"
+    config_end_flag = "# Github hosts end"
+    with open(smarddns_conf_path, 'r') as f:
+        lines = f.readlines()
+        new_lines = []
+        for line in lines:
+            line = line.strip()
+            if config_start_flag in line or config_end_flag in line:
+                continue
+            new_lines.append(line+"\n")
+
+    new_lines.append(config_start_flag + "\n")
+    for ip,host in host_ips:
+        new_lines.append(f"address /{host}/{ip}\n")
+    new_lines.append(config_end_flag + "\n")
+    with open(smarddns_conf_path, 'w') as f:
+        f.writelines(new_lines)
+    
+    # 重启smartdns
+    os.system("/etc/init.d/smartdns restart")
+    
+if __name__ == "__main__":
+    host_ips = get_all_host_ip()
     write_hosts(host_ips)
+    update_smartdns(host_ips)
+
